7555b3022434390f9b61159b6a3e040de60b4f5a
Allen <e1668058@cmaisonneuve.qc.ca>
2020-03-09 16:23:50 -0400

    Reglage du enqueue de main.js

--- functions.php
+++ functions.php
@@ -2,8 +2,16 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
+// function wp_enqueue_scripts() {
+//     wp_enqueue_script( 'script', get_template_directory_uri() . '/main.js', array ( 'jquery' ), 1.1, true);
+// }
+
+function main_js() {
+    wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/main.js', array ());
+}
+add_action('wp_enqueue_scripts', 'main_js');
+
 
 /* Permet d'adapter la requête principale avant qu'elle ne s'exécute */ 
 function extraire_evenement( $query ) {
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,6 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php underscores_post_thumbnail(); ?>
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
@@ -20,38 +21,35 @@
 		
 		if ( 'post' === get_post_type() ) :
 		?>
-			<div class="entry-meta">
-				<?php
-				underscores_posted_on();
-				underscores_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+
 		<?php endif; ?>
+
+			<div class="entry-content">
+			<?php
+			// the_content( sprintf(
+			// 	wp_kses(
+			// 		/* translators: %s: Name of current post. Only visible to screen readers */
+			// 		__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'underscores' ),
+			// 		array(
+			// 			'span' => array(
+			// 				'class' => array(),
+			// 			),
+			// 		)
+			// 	),
+			// 	get_the_title()
+			// ) );
+			echo '<p>'.substr(get_the_excerpt(),0,100) .'</p>';
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'underscores' ),
+				'after'  => '</div>',
+			) );
+			?>
+			<button class="bn">Lire la suite...</button>
+			</div><!-- .entry-content -->
+
 	</header><!-- .entry-header -->
 
-	<?php underscores_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'underscores' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'underscores' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+	
 
 	<footer class="entry-footer">
 		<?php underscores_entry_footer(); ?>
